c55887dff51c91729b51317a63b6d99bfde65961
sebastian-VB <sebas99peru@gmail.com>
2024-09-24 10:26:36 -0500

    Add routes - basic configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
