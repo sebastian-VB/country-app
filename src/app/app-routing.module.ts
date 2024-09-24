37744de63b46f1346f83d5337d55c4e166a7bb04
sebastian-VB <sebas99peru@gmail.com>
2024-09-24 11:06:51 -0500

    Add new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent
   },
   {
@@ -14,8 +15,12 @@ const routes: Routes = [
     component: AboutPageComponent
   },
   {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
     path: '**',
-    redirectTo: 'home'
+    redirectTo: ''
   },
 ];
 
