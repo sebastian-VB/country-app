d77a1c8247c8dac56f7f2e72732a0d15a639e160
sebastian-VB <sebas99peru@gmail.com>
2024-09-24 21:36:22 -0500

    Create interface for response of api

---
+++ src/app/countries/interfaces/country.interface.ts
@@ -0,0 +1,134 @@
+export interface Country {
+  name:         Name;
+  tld:          string[];
+  cca2:         string;
+  ccn3:         string;
+  cca3:         string;
+  cioc?:        string;
+  independent:  boolean;
+  status:       Status;
+  unMember:     boolean;
+  currencies:   { [key: string]: Currency };
+  idd:          Idd;
+  capital:      string[];
+  altSpellings: string[];
+  region:       string;
+  subregion:    string;
+  languages:    Languages;
+  translations: { [key: string]: Translation };
+  latlng:       number[];
+  landlocked:   boolean;
+  area:         number;
+  demonyms:     Demonyms;
+  flag:         string;
+  maps:         Maps;
+  population:   number;
+  fifa?:        string;
+  car:          Car;
+  timezones:    string[];
+  continents:   string[];
+  flags:        Flags;
+  coatOfArms:   CoatOfArms;
+  startOfWeek:  StartOfWeek;
+  capitalInfo:  CapitalInfo;
+  postalCode?:  PostalCode;
+  borders?:     string[];
+  gini?:        { [key: string]: number };
+}
+
+export interface CapitalInfo {
+  latlng: number[];
+}
+
+export interface Car {
+  signs: string[];
+  side:  Side;
+}
+
+export enum Side {
+  Left = "left",
+  Right = "right",
+}
+
+export interface CoatOfArms {
+  png?: string;
+  svg?: string;
+}
+
+export interface Currency {
+  name:   string;
+  symbol: string;
+}
+
+export interface Demonyms {
+  eng: Eng;
+  fra: Eng;
+}
+
+export interface Eng {
+  f: string;
+  m: string;
+}
+
+export interface Flags {
+  png:  string;
+  svg:  string;
+  alt?: string;
+}
+
+export interface Idd {
+  root:     string;
+  suffixes: string[];
+}
+
+export interface Languages {
+  eng?: string;
+  spa?: string;
+  por?: string;
+  pov?: string;
+  fra?: string;
+  nrf?: string;
+  ara?: string;
+  cal?: string;
+  cha?: string;
+  bos?: string;
+  hrv?: string;
+  srp?: string;
+  heb?: string;
+  pol?: string;
+  kon?: string;
+  lin?: string;
+  lua?: string;
+  swa?: string;
+  ita?: string;
+}
+
+export interface Maps {
+  googleMaps:     string;
+  openStreetMaps: string;
+}
+
+export interface Name {
+  common:     string;
+  official:   string;
+  nativeName: { [key: string]: Translation };
+}
+
+export interface Translation {
+  official: string;
+  common:   string;
+}
+
+export interface PostalCode {
+  format: string;
+  regex:  string;
+}
+
+export enum StartOfWeek {
+  Monday = "monday",
+  Sunday = "sunday",
+}
+
+export enum Status {
+  OfficiallyAssigned = "officially-assigned",
+}
