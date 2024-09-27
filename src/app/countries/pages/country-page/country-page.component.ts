aa95446916219420babd1ce252f0ad7dd758eee9
sebastian-VB <sebas99peru@gmail.com>
2024-09-27 13:59:30 -0500

    Add onInit method for consumer country service

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/country.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit{
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private countryService: CountriesService
+  ){}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id}) => this.countryService.searchCountryByAlphaCode(id))
+    )
+    .subscribe(
+      (country) => {
+        if(!country){
+          return this.router.navigateByUrl('');
+        }
+        return;
+      }
+    );
+    // this.activatedRoute.params
+    // .pipe(
+    // )
+    // .subscribe(
+    //   ({id}) => {
+    //     this.countryService.searchCountryByAlphaCode(id).subscribe(
+    //       (country) => {
+    //         console.log(country[0])
+    //       }
+    //     );
+    //   }
+    // );
+  }
 }
