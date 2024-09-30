4c61b6c0c8aa5cfc3c6686b6dc7ca57a176f5f46
sebastian-VB <sebas99peru@gmail.com>
2024-09-30 13:40:39 -0500

    Add refactoring in http request

--- src/app/countries/services/country.service.ts
+++ src/app/countries/services/country.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, delay, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({providedIn: 'root'})
@@ -11,6 +11,15 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
+  //en el pipe se captura el error y con of([]) retorna un nuevo observable
+  private getCountryRequest(url: string): Observable<Country[]>{
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(() => of([])),
+        delay(2000)
+      );
+  }
+
   searchCountryByAlphaCode(code: string): Observable<Country | null>{
     return this.http.get<Country[]>(`${this.apiUrl}/alpha/${code}`)
       .pipe(
@@ -19,26 +28,16 @@ export class CountriesService {
       );
   }
 
-  //en el pipe se captura el error y con of([]) retorna un nuevo observable
   searchCapital(capital: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${capital}`)
-      .pipe(
-        catchError(() => of([]))
-      );
+    return this.getCountryRequest(`${this.apiUrl}/capital/${capital}`);
   }
 
   searchCountry(country: string): Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiUrl}/name/${country}`)
-      .pipe(
-        catchError(() => of([]))
-      );
+    return this.getCountryRequest(`${this.apiUrl}/name/${country}`);
   }
 
   searchRegion(region: string): Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiUrl}/region/${region}`)
-      .pipe(
-        catchError(() => of([]))
-      );
+    return this.getCountryRequest(`${this.apiUrl}/region/${region}`);
   }
 
 }
