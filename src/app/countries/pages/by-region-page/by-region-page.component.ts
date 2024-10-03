7509bdda74356e8e189141519dd9a2a8a2c3bccb
sebastian-VB <sebas99peru@gmail.com>
2024-10-03 11:12:32 -0500

    Add Region type for better search

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/country.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,10 +12,15 @@ import { CountriesService } from '../../services/country.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas' , 'Asia' , 'Europe' , 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService){}
 
-  searchByRegion(term: string): void{
+  searchByRegion(term: Region): void{
+
+    this.selectedRegion = term;
+
     this.countriesService.searchRegion(term).subscribe(
       (countries) => this.countries = countries
     );
